1a66ca9dddeaf5066b058c526c368e96f0a04679
fahmi <Ahmadfahmi2@ce.student.pens.ac.id>
2022-01-11 10:55:59 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import Logo from './react.png';
+
+
+function App() {
+  return (
+    <div>
+      <img src={Logo} class ="icon" alt="Logo" />
+      <div>
+        <p class = 'bless'>
+          Selamat Datang Kembali !
+        </p>
+        <p>
+          Silahkan Masuk Akun Kakaedu Anda
+        </p>
+      </div>
+      <form>
+        <label>
+          <input class = "email" type="text" name="Alamat_Email" placeholder='Alamat Email' />
+        </label>
+      </form>
+
+      <form>
+        <label>
+          <input class = "pwd" type="text" name="Password" placeholder='Password' />
+        </label>
+      </form>
+      <input class = "submit" type="submit" name = "Masuk"/>
+    </div>
+   
+  )
+}
+
+export default App;
